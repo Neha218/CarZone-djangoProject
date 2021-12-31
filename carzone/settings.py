e77ef4a1033ed0b8183cd184379e71a82fc8894f
Neha Pawar <nehaapawar21@gmail.com>
2021-12-31 15:43:55 +0530

    Media files Configuration

--- carzone/settings.py
+++ carzone/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static file configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+# Media file configuration
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
